660d98551d5f2a9d394eabff24e2bfc93c030d82
trex22 <contact@jasonchalom.com>
2020-03-21 22:33:02 +0200

    Update namespace :wrench:

--- test/luno/client_test.rb
+++ test/luno/client_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class Luno::ClientTest < Minitest::Test
+class LunoTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Luno::Client::VERSION
+    refute_nil ::Luno::VERSION
   end
 
   def test_it_does_something_useful
