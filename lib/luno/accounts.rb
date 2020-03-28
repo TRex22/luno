7ad6ffea533ea7d2662e288b9b6a912f0ea8c745
trex22 <contact@jasonchalom.com>
2020-03-28 23:31:10 +0200

    Add in list account transactions method :hammer:

--- lib/luno/accounts.rb
+++ lib/luno/accounts.rb
@@ -1,8 +1,18 @@
 module Luno
   module Accounts
+    # GET paths
     def list_accounts
       path = 'accounts'
       authorise_and_send(http_method: :get, path: path)
     end
+
+    def list_account_transactions(account_id)
+      path = "accounts/#{account_id}/transactions"
+      authorise_and_send(http_method: :get, path: path)
+    end
+
+    # POST paths
+
+    # PUT paths
   end
 end
