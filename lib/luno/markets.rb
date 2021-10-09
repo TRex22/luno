c9d06632c59fd3cc616ac0f8157a7fb9f304c4e7
trex22 <contact@jasonchalom.com>
2021-10-09 15:48:59 +0200

    Add in ticker market requests :hammer:

--- lib/luno/markets.rb
+++ lib/luno/markets.rb
@@ -4,5 +4,15 @@ module Luno
       path = "orderbook_top?pair=#{pair}"
       unauthorised_and_send(http_method: :get, path: path)
     end
+
+    def list_ticker_for_pair(pair:)
+      path = 'ticker'
+      authorise_and_send(http_method: :get, path: path, params: { pair: pair })
+    end
+
+    def list_tickets_for_all_pairs
+      path = 'tickers'
+      authorise_and_send(http_method: :get, path: path)
+    end
   end
 end
--- lib/luno/version.rb
+++ lib/luno/version.rb
@@ -1,3 +1,3 @@
 module Luno
-  VERSION = "0.2.6"
+  VERSION = "0.2.7"
 end
